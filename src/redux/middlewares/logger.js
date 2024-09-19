0346c5b5a5f663b0e0e1c94e733b4d36f567d423
shihab01118 <shahed.shihab.0@gmail.com>
2024-09-19 18:25:37 +0600

    logger middleware added

---
+++ src/redux/middlewares/logger.js
@@ -0,0 +1,11 @@
+const logger = (store) => (next) => (action) => {
+    const currentState = store.getState();
+    console.log('Current State => ',currentState);
+    console.log('Action dispatched => ', action);
+
+    next(action);
+
+    console.log('Updated state => ', store.getState());
+}
+
+export default logger;
